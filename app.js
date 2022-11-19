b1fa28a12cb40fc5532e76b18e4782c4d3f16a5a
wego11ya <wego11ya@gmail.com>
2022-11-19 14:19:52 +0800

    Mongoose added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const app = express();
 const PORT = 3000;
+require("./config/mongoose");
 
 app.get("/", (req, res) => {
   res.send("hello world");
