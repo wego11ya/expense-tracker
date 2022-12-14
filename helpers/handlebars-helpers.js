822ea3953bf8ee61685755a7880225215fef9b48
wego11ya <wego11ya@gmail.com>
2022-12-15 07:18:40 +0800

    Create iconChoose handlebars helpers but failed, need to be fixed...

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,20 @@
+const Handlebars = require("handlebars");
+const iconDetails = {
+  1: ["家居物業", "fa-solid fa-house"],
+  2: ["交通出行", "fa-solid fa-van-shuttle"],
+  3: ["休閒娛樂", "fa-solid fa-face-grin-beam"],
+  4: ["餐飲食品", "fa-solid fa-utensils"],
+  5: ["其他", "fa-solid fa-pen"],
+};
+
+const iconChoose = Handlebars.registerHelper("iconChoose", function (num) {
+  const icon = iconDetails[num][1];
+  return icon;
+});
+
 module.exports = {
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this);
   },
+  iconChoose,
 };
