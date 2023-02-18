bbac50e9b9b7580bd2b9f88e139d9b2208642682
wego11ya <wego11ya@gmail.com>
2023-02-18 15:19:44 +0800

    feat: sorting by categoryId

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,7 +7,10 @@ const Record = require("../../models/record");
 // 定義首頁路由res.render("index");
 router.get("/", (req, res) => {
   const userId = req.user._id;
-  Record.find({ userId })
+  const categoryId = req.query.categoryId
+  const filter = (!categoryId || categoryId === '0' || categoryId === '6') ? { userId } : { userId, categoryId }
+
+  Record.find(filter)
     .lean()
     .sort({ date: "desc" })
     .then((records) => {
@@ -26,3 +29,11 @@ router.get("/", (req, res) => {
 });
 
 module.exports = router;
+
+
+{/* <option value="6">全部類別</option>
+<option value="1">家居物業</option>
+<option value="2">交通出行</option>
+<option value="3">休閒娛樂</option>
+<option value="4">餐飲食品</option>
+<option value="5">其他</option> */}
