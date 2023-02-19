5886d7a9efc653cf62678b8ccf4eca7cf8f44b0a
wego11ya <wego11ya@gmail.com>
2023-02-19 12:04:08 +0800

    try to fix selected option when selected but failed, needs to be fixed...

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,39 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
-// 引入Record medel
+// 引入models
 const Record = require("../../models/record");
+const Category = require("../../models/category")
 
 // 定義首頁路由res.render("index");
 router.get("/", (req, res) => {
-  const userId = req.user._id;
-  const categoryId = req.query.categoryId
-  const filter = (!categoryId || categoryId === '0' || categoryId === '6') ? { userId } : { userId, categoryId }
-
-  Record.find(filter)
+  const categories = []
+  const userId = req.user._id;  
+  const categoryId = req.query.sortByCategory
+  // 一開始categoryId是undefined，或是全部類別，就把所有資料撈出來; 否則就去撈前端傳回來的categoryId
+  const filter = (!categoryId || categoryId === '0') ? { userId } : { userId, categoryId }
+  
+  // 先把所有的category找出來再放到categories陣列，最後再傳回前端
+  Category.find()
     .lean()
-    .sort({ date: "desc" })
-    .then((records) => {
-      let totalAmount = 0;
-      records.forEach((record) => {
-        totalAmount += record.amount;
-        record.date = record.date.toLocaleDateString("ja-JP", {
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit",
+    .then(category => categories.push(...category))
+    .then(() => {
+      Record.find(filter)
+      .lean()
+      .sort({ date: "desc" })
+      .then((records) => {
+        let totalAmount = 0;
+        records.forEach((record) => {
+          totalAmount += record.amount;
+          record.date = record.date.toLocaleDateString("ja-JP", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+          });
         });
-      });
-      res.render("index", { records, totalAmount });
+        res.render("index", { records, totalAmount, categories });        
+      })
+      .catch((err) => console.log(err));
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
 });
-
 module.exports = router;
-
-
-{/* <option value="6">全部類別</option>
-<option value="1">家居物業</option>
-<option value="2">交通出行</option>
-<option value="3">休閒娛樂</option>
-<option value="4">餐飲食品</option>
-<option value="5">其他</option> */}
